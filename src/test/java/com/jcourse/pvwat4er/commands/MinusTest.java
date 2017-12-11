5a0646ff8aae538012afbcb07a654d4f8d767f86
pvwat4er <wat4er@gmail.com>
2017-12-12 00:22:29 +0700

    12.12.17

---
+++ src/test/java/com/jcourse/pvwat4er/commands/MinusTest.java
@@ -0,0 +1,10 @@
+package com.jcourse.pvwat4er.commands;
+
+import static org.junit.Assert.*;
+
+public class MinusTest {
+    @org.junit.Test
+    public void execute() throws Exception {
+    }
+
+}
---
+++ src/test/java/com/jcourse/pvwat4er/commands/MultTest.java
@@ -0,0 +1,15 @@
+package com.jcourse.pvwat4er.commands;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Stack;
+
+import static org.junit.Assert.*;
+
+public class MultTest {
+    @org.junit.Test
+    public void execute() throws Exception {
+    }
+
+}
